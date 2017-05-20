7af22ae06ddfad7075d1428334235579cc479f4c
Szymon Koper <sz.koper@gmail.com>
2017-05-20 16:15:57 +0200

    A wild widget appears. The layout is super ineffective (yet).

---
+++ app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
@@ -0,0 +1,10 @@
+package com.udacity.stockhawk.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by sakydpozrux on 20/05/2017.
+ */
+
+public class StockWidgetProvider extends AppWidgetProvider {
+}
